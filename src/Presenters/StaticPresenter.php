084c6c9024cb4e52510c0f4ae21782114e069f7f
Filip Procházka <filip@prochazka.su>
2017-05-01 01:16:38 +0200

    use paypal.me links

--- src/Presenters/StaticPresenter.php
+++ src/Presenters/StaticPresenter.php
@@ -5,24 +5,9 @@ namespace Fp\Presenters;
 class StaticPresenter extends BasePresenter
 {
 
-	/** @var string */
-	private $paypalKeyFile;
-
-	public function __construct(string $paypalKeyFile)
-	{
-		parent::__construct();
-
-		$this->paypalKeyFile = $paypalKeyFile;
-	}
-
 	public function actionTalk(string $talk)
 	{
 		$this->setView('talks/' . $talk);
-	}
-
-	public function renderDonation()
-	{
-		$this->template->paypalKey = file_get_contents($this->paypalKeyFile);
 	}
 
 	protected function beforeRender()
